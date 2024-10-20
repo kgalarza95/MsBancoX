2ee3dc143119dc2f5f1ee9f15cc58e878e2d35c7
kgalarza <kevin_galata@hotmail.com>
2024-10-20 01:24:05 -0500

    init del proyecto de gestión bancaria con el ms de gestión clientes

---
+++ MsGestionClientesX/src/main/java/com/kgalarza/cliente/msclientesx/MsClientesXApplication.java
@@ -0,0 +1,13 @@
+package com.kgalarza.cliente.msclientesx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClientesXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClientesXApplication.class, args);
+	}
+
+}
